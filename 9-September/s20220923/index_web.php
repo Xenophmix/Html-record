6485a7b7b97048abf4378e0b895ff28c4b18dc32
Xenophmix <xenophmix@gmail.com>
2022-12-07 09:58:51 +0800

    根據月份整理目前為止的學習檔案

---
+++ 9-September/s20220923/index_web.php
@@ -0,0 +1,26 @@
+<?php
+$book=10;
+echo "book" . $book;
+
+echo "<br />";
+$name="廖凱鴻";
+echo "我的名字是" .$name;
+
+$second=60*60*24;
+echo "<br/>";
+echo "一天有".$second."秒";
+echo "<br />";
+$name="廖先生";
+echo "我的名字是" .$name;
+
+define("COMPANY","泰山訓練場");
+
+echo COMPANY.$name;
+?>
+
+
+<h2>運算子</H2>
+<?php
+$op=true;
+
+?>
